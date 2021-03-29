837ef481055c5d084022453145cf293b3464fa74
SolMarina <sparraa17@gmail.com>
2021-03-28 22:04:55 -0300

    Adelanto de estilo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { NutrialusApp } from './components/NutrialusApp';
+import { NutrialusApp } from './components/nutrialus-app/NutrialusApp';
+import '../src/index.css'
+
 
 
 ReactDOM.render(
+  <div className='bla'>
+    
+    
 
-  <NutrialusApp />,
-
+  <NutrialusApp />
+  </div>,
   document.getElementById('root')
 );
 
